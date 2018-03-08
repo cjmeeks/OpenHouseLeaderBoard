f8aabb320d52c1025d5c9c100464b5432c3aaacd
Clifford Meeks <cliffmeeks2214@gmail.com>
2018-03-08 12:37:34 -0600

    finished basic setup

--- leaderboard/app.js
+++ leaderboard/app.js
@@ -1,4 +1,14 @@
 var express = require('express');
+
+//mongo
+var mongoose = require('mongoose');
+var employees = require('./routes/employees');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/product')
+  .then(() =>  console.log('connection succesful'))
+  .catch((err) => console.error(err));
+
+  
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -7,14 +17,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var mongoose = require('mongoose');
 
-
-mongoose.Promise = global.Promise;
-
-mongoose.connect('mongodb://localhost/product')
-  .then(() =>  console.log('connection succesful'))
-  .catch((err) => console.error(err));
 
 var app = express();
 
@@ -32,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/employees', employees);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
