51e8560193fc2b5092b6f5039293f60e61622969
Ryan Williams <rpwilliams96@gmail.com>
2018-04-04 16:38:15 +0200

    Voting page tweaks (#5)
    * Changed scroll speed
    
    * Idk
    
    * Sorted cards alphabetically

--- controllers/DisplayController.js
+++ controllers/DisplayController.js
@@ -4,90 +4,99 @@ var Display = require("../models/Display");
 
 var displayController = {};
 
+/* Card Data */
 displayController.show = function(req, res) {
     Display.find({}).exec(function (err, displays) {
       if (err) {
         console.log("Error:", err);
       }
       else {
+        /* Sort the displays alphabetically */
+        displays.sort(function(a, b){
+          if(a.name < b.name) return -1;
+          if(a.name > b.name) return 1;
+          return 0;
+        });
         res.render("../views/vote", {displays: displays});
       }
     });
-  };
+};
 
+/* Leaderboard Data */
 displayController.leaderboard = function(req, res) {
   Display.find({}).exec(function (err, displays) {
     if (err) {
       console.log("Error:", err);
     }
     else {
+      /* Sort by number of votes */
       displays.sort(function(a, b){
         return b.votes-a.votes
       });
       res.render("../views/leaderboard", {displays: displays});
     }
   });
-  };
+};
 
-  displayController.create = function(req, res) {
-    res.render("../views/displays/create");
-  };
+displayController.create = function(req, res) {
+  res.render("../views/displays/create");
+};
 
-  displayController.save = function(req, res) {
-    var display = new Display(req.body);
-  
-    display.save(function(err) {
-      if(err) {
-        console.log(err);
-        res.render("../views/displays/create");
-      } else {
-        console.log("Successfully created an display.");
-        res.redirect("/");
-      }
-    });
-  };
+displayController.save = function(req, res) {
+  var display = new Display(req.body);
 
-  displayController.voteForOneDisplay = function(req, res){
-    Display.findOne({_id: req.params.id}).exec(function (err, display) {
-      if (err) {
-        console.log("Error:", err);
-      }
-      else {
-        if(display.votes){
-          display.votes++;
-        }
-        else {
-          display.votes = 1;
-        }
-        display.save(function(err) {
-          if(err) {
-            console.log(err);
-          } else {
-            console.log("Successfully Voted an display.");
-            res.redirect("/submitted/" + display.name);
-          }
-        });
-      }
-    });
-  };
+  display.save(function(err) {
+    if(err) {
+      console.log(err);
+      res.render("../views/displays/create");
+    } else {
+      console.log("Successfully created an display.");
+      res.redirect("/");
+    }
+  });
+};
 
-  displayController.clear = function(req, res){
-    Display.find({}).exec(function (err, displays) {
-      if (err) {
-        console.log("Error:", err);
+displayController.voteForOneDisplay = function(req, res){
+  Display.findOne({_id: req.params.id}).exec(function (err, display) {
+    if (err) {
+      console.log("Error:", err);
+    }
+    else {
+      if(display.votes){
+        display.votes++;
       }
       else {
-          displays.forEach(function(display){
-            display.votes = 0;
-            display.save(function(err) {
-              if(err) {
-                console.log(err);
-              }
-            });
+        display.votes = 1;
+      }
+      display.save(function(err) {
+        if(err) {
+          console.log(err);
+        } else {
+          console.log("Successfully Voted an display.");
+          res.redirect("/submitted/" + display.name);
+        }
+      });
+    }
+  });
+};
+
+displayController.clear = function(req, res){
+  Display.find({}).exec(function (err, displays) {
+    if (err) {
+      console.log("Error:", err);
+    }
+    else {
+        displays.forEach(function(display){
+          display.votes = 0;
+          display.save(function(err) {
+            if(err) {
+              console.log(err);
+            }
           });
-          res.redirect("/leaderboard");
-          }
         });
-  };
+        res.redirect("/leaderboard");
+        }
+      });
+};
 
-  module.exports = displayController;
+module.exports = displayController;
