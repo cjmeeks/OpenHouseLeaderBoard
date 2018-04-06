669e98521f1ce045bc4a6fa7940fe5dcb5cd71b5
Daniel Lovell <dannylovell11@gmail.com>
2018-04-06 18:55:17 -0500

    Update leaderboard to use Normal Competition Ranking scheme. (#8)

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var http = require('http');
+var path = require('path');
 
 const PORT = 3001;
 
+var prevLeaderBoardRank = 0;
 
 //mongo
 var mongoose = require('mongoose');
@@ -60,5 +62,47 @@ var server = http.createServer(app);
 server.listen(PORT, function() {
 	console.log("Server listening on port " + PORT);
 });
+
+/* Leaderboard content functions. */
+
+/* If the next display in the leaderboard has the same
+   number of votes as the previous, make the ranking
+   reflect the tie. */
+app.locals.getRank = function(orderIndex, displayArr){
+  var curLeaderBoardRank = orderIndex;
+
+  if(orderIndex > 1){
+    if (displayArr[i-1].votes == displayArr[i-2].votes){
+      curLeaderBoardRank = prevLeaderBoardRank;
+    }    
+  }
+  prevLeaderBoardRank = curLeaderBoardRank;
+
+  return curLeaderBoardRank;
+}
+
+app.use(express.static('public'));
+
+app.locals.getTrophyImage = function(rank, index, displayArr){
+  /* Trophies */
+	switch(rank){
+    case 1:
+      return "/images/trophies/trophy_gold";
+      
+    case 2:
+      return "/images/trophies/trophy_silver";
+
+    case 3:
+      return "/images/trophies/trophy_bronze";
+
+    default:
+      return function(index, displayArr){
+        if(index==displayArr.length){
+          //future code for adding poop emoji?
+          return;
+        }
+      };
+  }
+}
 
 module.exports = app;
